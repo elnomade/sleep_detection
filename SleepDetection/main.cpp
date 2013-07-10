89f39b69fa3ac6bc0d0c44221d53ff2077aa6c1d
matiasfg <matiasfg@gmail.com>
2013-07-10 17:53:47 -0300

    + Added detection level indicator. - Improved property sheets.

--- SleepDetection/main.cpp
+++ SleepDetection/main.cpp
@@ -2,11 +2,12 @@
 #include <opencv2/imgproc/imgproc.hpp>
 #include <opencv2/objdetect/objdetect.hpp>
 
+#include <iostream>
 #include <string>
 
-class DetectFaces {
+class DetectClosestFace {
 public:
-	DetectFaces( void )
+	DetectClosestFace( void )
 		: m_faceCascade(),
 		m_eyesCascade()
 	{
@@ -27,16 +28,20 @@ public:
 		cv::cvtColor(src, gray, CV_BGR2GRAY);
 		cv::equalizeHist(gray, gray);
 
-		typedef std::vector<cv::Rect> Faces, Eyes;
+		typedef std::vector<cv::Rect> Faces;
 
 		Faces faces;
 		m_faceCascade.detectMultiScale(gray, faces, 1.1, 3, CV_HAAR_SCALE_IMAGE|CV_HAAR_FIND_BIGGEST_OBJECT, cv::Size(30, 30));
+
+		unsigned int alarmLevel = 0;
 
 		std::for_each(
 			faces.cbegin(),
 			faces.cend(),
 			[&](const Faces::value_type & face)
 		{
+			++alarmLevel;
+
 			cv::ellipse(
 				src,
 				cv::Point(face.x + face.width*0.5, face.y + face.height*0.5),
@@ -49,10 +54,14 @@ public:
 				8,
 				0);
 
-			const auto && faceROI = gray(face);
+			const cv::Mat && faceROI = gray(face);
+
+			typedef std::vector<cv::Rect> Eyes;
 
 			Eyes eyes;
 			m_eyesCascade.detectMultiScale(faceROI, eyes, 1.1, 2, CV_HAAR_SCALE_IMAGE, cv::Size(30, 30));
+
+			alarmLevel += eyes.size() > 1 ? 1 : 0;
 
 			std::for_each(
 				eyes.cbegin(),
@@ -69,6 +78,20 @@ public:
 					0);
 			});
 		});
+
+		cv::Scalar alarmColors[] = {
+			cv::Scalar(  0.0,   0.0, 255.0), // RED		-	Detected nothing
+			cv::Scalar(  0.0, 255.0, 255.0), // YELLOW	-	Detected face and one or more eyes
+			cv::Scalar(  0.0, 255.0,   0.0), // GREEN	-	Detected face and both eyes
+		};
+
+		if (alarmLevel > 2) alarmLevel = 2; // clamp it in case of a Buddha eye.
+
+		cv::rectangle(
+			src,
+			cv::Rect(0, 0, src.size().width, src.size().height),
+			alarmColors[alarmLevel],
+			4);
 	}
 
 private:
@@ -78,16 +101,28 @@ private:
 };
 
 template <class Process>
-void CaptureWebCam( Process && proc, const std::string & name )
+void CaptureWebCam( Process && proc, const std::string & name = "" )
 {
-	cv::namedWindow(name, CV_WINDOW_AUTOSIZE);
-
 	cv::VideoCapture video(0);
+
+	if (!video.isOpened())
+	{
+		std::cerr << "Could not open video device 0";
+
+		return;
+	}
 
 	cv::Mat image;
 	for (;;)
 	{
 		video >> image;
+
+		if (image.empty())
+		{
+			std::cout << "No video input detected, exiting.";
+
+			break;
+		}
 
 		proc(image);
 
@@ -103,5 +138,5 @@ void CaptureWebCam( Process && proc, const std::string & name )
 
 int main( int, char *[] )
 {
-	CaptureWebCam(DetectFaces(), "Detecting faces");
+	CaptureWebCam(DetectClosestFace(), "Detecting closest face");
 }
